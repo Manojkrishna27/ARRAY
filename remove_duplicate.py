6082c3d5ae85c612b5eddcc7b31baea3dddadd7e
Manojkrishna27 <manojkrishna2725@gmail.com>
2025-12-11 23:56:39 +0530

    removing duplicates values

--- remove_duplicate.py
+++ remove_duplicate.py
@@ -1,12 +1,12 @@
 def removeduplicate(arr):
     n=len(arr)
-    result=set([]) # with use of set we can remove the duplicate because set does not contain any duplicates values
+    res=set([]) # with use of set we can remove the duplicate because set does not contain any duplicates values
     
     for i in range(n):
 
-        result.add(arr[i])
+        res.add(arr[i])
 
-    return result
+    return res
 arr=[1,2,3,4,6,6,4]
 print(removeduplicate(arr))
 
