1651ff7bd750c1b22567221fe920ec47b732fbd2
Manojkrishna27 <manojkrishna2725@gmail.com>
2025-12-12 11:22:50 +0530

    rotate_left_by_one

--- rotate_an_array_leftby_one.py
+++ rotate_an_array_leftby_one.py
@@ -12,3 +12,7 @@ def rotate_left_by_one(arr):
 arr=[1,2,3,4]
 print(rotate_left_by_one(arr))
  
+# super short technique but not recommended for the interview
+arr1=[1,4,2,3]
+arr1.append(arr1.pop(0))  # pop is removing the index 0 element and that element is append to the last of a list
+print(arr1)
