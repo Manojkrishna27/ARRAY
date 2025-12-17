0448ada776b901980fabaa88c4f8d1b63cad1e0b
Manojkrishna27 <manojkrishna2725@gmail.com>
2025-12-17 18:23:58 +0530

    upperbound

---
+++ upperbound.py
@@ -0,0 +1,21 @@
+"""Given a sorted array of nums and an integer x, write a program to find the upper bound of x.
+The upper bound of x is defined as the smallest index i such that nums[i] > x.
+If no such index is found, return the size of the array."""
+
+def upperbound(arr,target):
+    low=0
+    high=len(arr)-1
+    ans=len(arr)
+
+    while low<=high:
+        mid=(low+high)//2
+
+        if arr[mid]>target: # symbol is logic for upper bound
+            ans=mid         #store
+            high=mid-1      #left
+        else:
+           low=mid+1        # right
+    return ans              # returns index value
+arr=[1,3,4,5]
+target=2    
+print(upperbound(arr,target))
