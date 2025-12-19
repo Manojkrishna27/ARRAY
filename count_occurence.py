0306a08bb5222385694f52b7db6cf280943addf9
Manojkrishna27 <manojkrishna2725@gmail.com>
2025-12-19 19:01:49 +0530

    count_occurence

---
+++ count_occurence.py
@@ -0,0 +1,35 @@
+def countoccurence(arr,target):
+    low=0
+    n=len(arr)
+    lb=n
+    high=n-1
+    # lower bound
+    while low<=high:
+        mid=(low+high)//2
+        if arr[mid]>=target:
+            lb=mid
+            high=mid-1
+        else:
+            low=mid+1
+     # checking the edge if target not found       
+    if lb==n or arr[lb]!=target:
+        return 0 
+    # reset the element
+    #upper bound
+    low=0
+    n=len(arr)
+    high=n-1
+    ub=n
+    while low<=high:
+        mid=(low+high)//2
+        if arr[mid]>target:
+            ub=mid
+            high=mid-1
+        else:
+            low=mid+1
+    return ub-lb # ub-lb =count here ub is 3 index(4) and lb is 2 index(1) 4-1 =count 3
+arr=[1,2,2,2,3]
+target=2
+print(countoccurence(arr,target))
+
+
