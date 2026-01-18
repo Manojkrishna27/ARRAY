b5e0b6354c0684f359aec22e3a58401c345b24da
Manojkrishna27 <manojkrishna2725@gmail.com>
2026-01-18 23:46:34 +0530

    max subarray

--- max_sub_array.py
+++ max_sub_array.py
@@ -8,6 +8,6 @@ def maxsubarray(arr,k):
         window=window-arr[i-k]+arr[i]  # add next and remove the first element this process goes on
         max_array=max(window,max_array) # it returns the max of subarrays
     return max_array
-arr=[2,1,5,1,3,2]
+arr=[2,1,5,1,8,2]
 k=3
 print(maxsubarray(arr,k))
