6455e332d49eb6d4b0cd43d80d163e71ee5b99b4
Manojkrishna27 <manojkrishna2725@gmail.com>
2026-01-31 23:10:43 +0530

    REVERSE A NUMBER

--- reverse_num.py
+++ reverse_num.py
@@ -1,10 +1,10 @@
 def reverse_number(num):
     n=num
-    digits=0
+    digit=0
     rev=0
     while n>0:
-        digits=n%10 # store last digit
-        rev=rev*10+digits # add that digits to rev
+        digit=n%10 # store last digit
+        rev=rev*10+digit # add that digits to rev
         n//=10
     return rev
 num=123
